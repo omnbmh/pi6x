1fddc1c05a25cc48703d079c2fa4cf7e774dfebf
dezhichen <c8d8z8@gmail.com>
2015-02-06 15:45:29 +0800

    developing adjustment

---
+++ webapps/event54/views.py
@@ -0,0 +1,34 @@
+﻿from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django import template
+from django.template.loader import get_template
+
+import urllib
+import json
+
+# Create your views here.
+
+def index(req):
+    data = {}
+    if 'event54' in req.session:
+        data = req.session['event54']
+    t = get_template('event54.html')
+    c = template.Context(data)
+    return HttpResponse(t.render(c))
+    
+def authorize(req):
+    uid = req.GET.get('uid')
+    postDict = {
+        'app_key':100002,
+        'app_secret':'ef1302e559c65f91864ada2ff1fe6b27',
+        'uid':uid
+    }
+    postData = urllib.urlencode(postDict);
+    request = urllib2.Request('http://192.168.8.3:8080/sos-api/oauth2/access_token',postData)
+    request.add_header('Content-Type', "application/x-www-form-urlencoded")
+    s = urllib2.urlopen(request).read()
+    j = json.loads(s)
+    # print j['result']
+    # wirite session
+    req.session['event54'] = j['result']
+    return HttpResponseRedirect('./')
