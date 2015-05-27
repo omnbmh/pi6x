c39a508d54e52f07aaa15549e67bb45a526b56ea
omnbmh <c8d8z8@gmail.com>
2015-05-27 22:43:08 +0800

    删除文件

--- python3/Notepad++Test.py
+++
@@ -1,5 +0,0 @@
-import platform;
-
-print ("Just for demo how to python development under windows;");
-print ("Current python version info is %s"%(platform.python_version()));
-print ("uname=",platform.uname());
--- python3/ossystem.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-hha = os.system('ping 10.105.77.1')
-print('-------------------------------------')
-print(hha)
