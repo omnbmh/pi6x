6ce81918ee1ba515e91e22ea55e6dda3354083d0
dezhichen <c8d8z8@gmail.com>
2015-10-07 19:55:03 +0800

    四轮子驱动

--- hardware/arduino_uno/iot-car/code/code.ino
+++ hardware/arduino_uno/iot-car/code/code.ino
@@ -9,34 +9,47 @@
 * Modified 2015年10月07日
 * by 陈德志
 *
+* 引脚
+* Arduino Uno
+* 模拟输入 3 5 6 9 10 11
+* 模拟输出 A0 A1 A2 A3 A4 A5
+*
+* 74HC595D 引脚
+* pin 11 Clock Pin SH_CP 8
+* pin 12 Latch Pin ST_CP 7
+* pin 14 data pin DS 4
+* Vcc pin 16 & MR pin 10 接 5V
+* GND pin 8 & OE pin 13 接 地
 */
 
 // #include
+int ledPin = 13;
 
+byte test595Data = B10111111;
 int forwordData = 170; // 10101010
 int backData = 85; // 01010101
 int leftData = 102; // 01100110
 int rightData = 153; // 10011001
 
-int enable1Pin = 11; //M1 Speed Control
-int enable2Pin = 10; //M2 Speed Control
-int enable3Pin = 9; // M3 Speed Control
-int enable4Pin = 8; // M4 Speed Control
+int enable1Pin = 10; //M1 Speed Control
+int enable2Pin = 9; //M2 Speed Control
+int enable3Pin = 6; // M3 Speed Control
+int enable4Pin = 5; // M4 Speed Control
 
-int latchPin = 7;
-int dataPin = 6;
-int clockPin = 5;
+int clockPin = 8;
+int latchPin = 7; //
+int dataPin = 3;
 
 
 
-// 调整速度
-int potPin = 0;
+// 速度
+int speed = 234;
 
-void setMotor() {
-  analogWrite(enable1Pin, 230);
-  analogWrite(enable2Pin, 230);
-  analogWrite(enable3Pin, 230);
-  analogWrite(enable4Pin, 230);
+void setMotorSpeed(int speed) {
+  analogWrite(enable1Pin, speed);
+  analogWrite(enable2Pin, speed);
+  analogWrite(enable3Pin, speed);
+  analogWrite(enable4Pin, speed);
 }
 
 void setup() {
@@ -45,31 +58,48 @@ void setup() {
   pinMode(enable2Pin, OUTPUT);
   pinMode(enable3Pin, OUTPUT);
   pinMode(enable4Pin, OUTPUT);
+
+  // 设置 74HC595D 引脚
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
   // light led
-  pinMode(13, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  //int speed = analogRead(potPin)/4;
-  // 设置 电机的速度 和 方向
-  setMotor();
+  setMotorSpeed(speed);
+
+  //shiftOut(test595Data);
+  //for (int i = 0; i < 256 ; i++) {
+  digitalWrite(latchPin, LOW);
+  //shiftOut(i);
+  shiftOut(forwordData);
+  //shiftOut(test595Data);
+  digitalWrite(latchPin, HIGH);
   //delay(1000);
+  //}
   // light led
-  digitalWrite(13, HIGH);
+  digitalWrite(ledPin, HIGH);
 }
 
 void shiftOut(byte data) {
+
+
+  boolean pinState ;
   digitalWrite(dataPin, LOW);
   digitalWrite(clockPin, LOW);
   for (int i = 0; i <= 7; i++) {
     digitalWrite(clockPin, LOW);
     if (data & (1 << i)) {
-      digitalWrite(dataPin, HIGH);
+      pinState = HIGH;
     } else {
-      digitalWrite(dataPin, LOW);
+      pinState = LOW;
     }
-    digitalWrite(clockPin, LOW);
+    digitalWrite(dataPin, pinState);
+    digitalWrite(clockPin, HIGH);
   }
+  digitalWrite(clockPin, LOW);
 }
 
 
--- hardware/arduino_uno/学习实例/017.74HC595_8_eds/code/code.ino
+++ hardware/arduino_uno/学习实例/017.74HC595_8_eds/code/code.ino
@@ -1,37 +1,37 @@
-int latchPin =8; // 74HC595 12
-int clockPin = 12;// 74HC595 11
-int dataPin = 11;// 74HC595 14
+int latchPin = 7; // 74HC595 12
+int clockPin = 8;// 74HC595 11
+int dataPin = 3;// 74HC595 14
 
-void setup(){
-  pinMode(latchPin,OUTPUT);
-  pinMode(clockPin,OUTPUT);
-  pinMode(dataPin,OUTPUT);
+void setup() {
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
 }
 
-void loop(){
-  for(int i =0; i< 256 ;i++){
-    digitalWrite(latchPin,LOW);
+void loop() {
+  for (int i = 0; i < 256 ; i++) {
+    digitalWrite(latchPin, LOW);
     shiftOut(i);
-    digitalWrite(latchPin,HIGH);
+    digitalWrite(latchPin, HIGH);
     delay(1000);
   }
 }
 
-void shiftOut(byte data){
+void shiftOut(byte data) {
   boolean pinState;
-  digitalWrite(dataPin,LOW);
-  digitalWrite(clockPin,LOW);
-  for(int i =0; i<=7;i++){
-    digitalWrite(clockPin,LOW);
-    if (data & (1<<i)){
+  digitalWrite(dataPin, LOW);
+  digitalWrite(clockPin, LOW);
+  for (int i = 0; i <= 7; i++) {
+    digitalWrite(clockPin, LOW);
+    if (data & (1 << i)) {
       pinState = HIGH;
     }
-    else{
-      pinState= LOW;
+    else {
+      pinState = LOW;
     }
-    digitalWrite(dataPin,pinState);
-    digitalWrite(clockPin,HIGH);
+    digitalWrite(dataPin, pinState);
+    digitalWrite(clockPin, HIGH);
   }
-  digitalWrite(clockPin,LOW);
+  digitalWrite(clockPin, LOW);
 }
 
